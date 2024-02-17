7d31089e5f186baf2faf0b209a8f8a6639d4d5ca
nrashmi06 <rashminattoji24@gmail.com>
2024-02-17 12:29:12 +0530

    intergration1

--- GroceryApp/src/main/java/com/Hackloop/GroceryApp/controller/AdminGroceryItemController.java
+++ GroceryApp/src/main/java/com/Hackloop/GroceryApp/controller/AdminGroceryItemController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @Slf4j
 public class AdminGroceryItemController {
 

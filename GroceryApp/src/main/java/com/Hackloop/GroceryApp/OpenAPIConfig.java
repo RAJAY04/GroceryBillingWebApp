b84b71c7918b01689a6ca945eab682404cc2493b
ajayp <ajaprabhu2004@gmail.com>
2024-02-20 22:53:58 +0530

    added doccumentation uring swagger

---
+++ GroceryApp/src/main/java/com/Hackloop/GroceryApp/OpenAPIConfig.java
@@ -0,0 +1,16 @@
+package com.Hackloop.GroceryApp;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Grocery App",
+                version = "1.0.0",
+                summary = "It is a simple grocery app API. It is used to manage grocery items and generate bills.",
+                description = "Grocery App API"
+        )
+)
+
+public class OpenAPIConfig {
+
+}

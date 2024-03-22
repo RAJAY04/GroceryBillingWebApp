a0bc8a2ece13e5a595a8aaddc4f4f47a86077654
ajayp <ajaprabhu2004@gmail.com>
2024-03-22 22:49:27 +0530

    added billing feature

--- GroceryApp/src/main/java/com/Hackloop/GroceryApp/dto/BillItemDTO.java
+++ GroceryApp/src/main/java/com/Hackloop/GroceryApp/dto/BillItemDTO.java
@@ -1,15 +1,2 @@
-package com.Hackloop.GroceryApp.dto;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
-
-@Data
-@NoArgsConstructor
-public class BillItemDTO {
-    private Long itemId;
-    private String name;
-    private int quantity;
-    private BigDecimal price;
+package com.Hackloop.GroceryApp.dto;public class BillItemDto {
 }

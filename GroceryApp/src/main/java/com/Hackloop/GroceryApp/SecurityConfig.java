17bd2be4593a19b14c89ad7d2b23a0d5747477fc
ajayp <ajaprabhu2004@gmail.com>
2024-03-22 19:20:20 +0530

    fixed errors forbidden

--- GroceryApp/src/main/java/com/Hackloop/GroceryApp/SecurityConfig.java
+++ GroceryApp/src/main/java/com/Hackloop/GroceryApp/SecurityConfig.java
@@ -34,7 +34,7 @@ public class    SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeRequests((authorize) ->
-                        authorize.requestMatchers(UrlMapping.BASE_API + "/").permitAll()
+                        authorize.requestMatchers(UrlMapping.BASE_API + "/**").permitAll()
                                 .anyRequest().authenticated()
                 );
         return http.build();
